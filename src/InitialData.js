5cc95f86638de9421aba5c53b0e424ef8804f2df
PRATIMA <107994434+PRATIMASINGH123@users.noreply.github.com>
2022-10-04 17:43:26 +0530

    Update InitialData.js

--- src/InitialData.js
+++ src/InitialData.js
@@ -1,41 +1,87 @@
-const mongoose = require('mongoose');
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+let studentArr = require("./InitialData");
+const studentArray = require("./InitialData");
+const port = 8080;
+app.use(express.urlencoded());
 
-// uncomment codes 
-
-const studentSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    sex: {
-        type: String,
-        required: true,
-    },
-    age: {
-        type: Number,
-        required: true,
-    },
-    class: {
-        type: String,
-        required: true,
-    },
-    grade_point:{
-        type: Number,
-        required: true
-    },
-    time:{
-        type: Date,
-        default: Date.now,
-        required: true
-    },
-    isDeleted:{
-        type: Boolean,
-        default: false,
-        required: true,
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// your code goes here
+let maxId = studentArr.length;
+app.get("/api/student", (req, res) => {
+  res.send(studentArr);
+});
+app.get("/api/student/:id", (req, res) => {
+    const idToSearch=req.params.id
+    const matched=studentArray.filter((student)=>student.id===Number(idToSearch))
+    if(matched.length===0){
+        res.sendStatus(404)
+    }else{
+        res.send(matched[0])
     }
-})
+  });
+  const isNullOrUndefined=(val)=>val===null|| val===undefined;
+  app.post('/api/student',(req,res)=>{
+      const newStudent=req.body
+      const {name,currentClass,division}=newStudent
+      if(isNullOrUndefined(name)||isNullOrUndefined(currentClass)||isNullOrUndefined(division)){
+          res.sendStatus(404)
+      }else{
+          const newId=maxId+1
+          maxId=newId
+          newStudent.id=newId
+          newStudent.currentClass=Number(currentClass)
+          studentArray.push(newStudent)
+          res.send({id:newId})
+
+        }
+  })
+  app.put('/api/student/:id',(req,res)=>{
+    const idToSearch=req.params.id
+    const update=req.body
+    const {name,currentClass,division}=update
+    const matchedIdx=studentArray.findIndex((student)=>student.id===Number(idToSearch))
+    if(matchedIdx===-1){
+        res.sendStatus(400)
+    }
+    else{
+        if(
+            isNullOrUndefined(name) && 
+            isNullOrUndefined(currentClass) && 
+            isNullOrUndefined(division)){
+            res.sendStatus(400)
+        }
+        else{
+            if(!isNullOrUndefined(name)){
+                studentArray[matchedIdx].name=name
+            }
+            if(!isNullOrUndefined(currentClass)){
+            studentArray[matchedIdx].currentClass=Number(currentClass)
+           }
+           if(!isNullOrUndefined(division)){
+            studentArray[matchedIdx].division=division
+        }
+        res.sendStatus(200)
+        }
+    }
+  })
+  app.delete('/api/student/:id',(req,res)=>{
+    const idToSearch=req.params.id
+    const matchedIdx=studentArray.findIndex((student)=>student.id===Number(idToSearch))
+    if(matchedIdx===-1){
+        res.sendStatus(404)
+    }else{
+        studentArray.splice(matchedIdx,1)
+        res.sendStatus(200);
+    }
+
+  })
 
 
-const studentModel = mongoose.model('students', studentSchema);
+app.listen(port, () => console.log(`App listening on port ${port}!`))
 
-module.exports = studentModel;
+module.exports = app;
